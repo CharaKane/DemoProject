ce7eb6ef038e744e534398448d4d0dd265e6688e
CharaKane <117196115+CharaKane@users.noreply.github.com>
2023-03-15 18:07:12 +0200

    cpp

--- askisi2.cpp
+++ askisi2.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 
-using namespace std;
-
+ using namespace std;
+ 
 int main(){
-  int x,y ;
-    cout << "dose duo arithmous:";
-    cin >> x;
-    cin >> y;
-	
-	cout << "To athrisma ton arithmon einai:" <<  (x+y) << endl;
-    cout << "To ginomeno ton arithmon einai:" <<  (x*y)  << endl;
+  double number;
 
-    return 0;
+  cout << "dose enan arithmo:\n" << endl;
+  cin >> number;
+
+if(number<0.0) 
+ 
+ cout << "dosate arnitiko arithmo" << endl;
+
+ else if (number>0.0)
+
+ cout << "dosate thetiko arithmo" << endl;
+
+ else 
+
+ cout << "dosate 0" << endl;
+
+ return 0;
 }
